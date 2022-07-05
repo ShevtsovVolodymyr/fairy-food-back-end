a8b74065e4e655b98e05552c83b5b84928531233
v.shevtsov <versaptr@gmail.com>
2022-07-05 17:36:21 +0300

    updated: company model; added: dev script

--- models/company.js
+++ models/company.js
@@ -2,16 +2,24 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const companySchema = new Schema({
-	name: {
-		type: String,
-		required: true,
-	},
-	products: [
-		{
-			type: mongoose.Types.ObjectId,
-			required: true,
-			ref: 'Product',
-		},
-	],
+  name: {
+    type: String,
+    required: true,
+  },
+  location: {
+    lat: {
+      type: Number,
+    },
+    lng: {
+      type: Number,
+    },
+  },
+  products: [
+    {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: 'Product',
+    },
+  ],
 });
 module.exports = mongoose.model('Company', companySchema);
